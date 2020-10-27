59e4ee757377250f2c7999dc430c89a7d4cf3860
Heso113 <hampe_olsson91@hotmail.com>
2020-10-27 10:20:43 +0100

    new features
    added:
    -new model called Comment
    -new repository called CommentRepository
    -new controller called CommentController

---
+++ src/main/java/experis/noticeboard/controllers/api/CommentController.java
@@ -0,0 +1,111 @@
+package experis.noticeboard.controllers.api;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import experis.noticeboard.models.Comment;
+import experis.noticeboard.repositories.CommentRepository;
+
+
+@RestController
+public class CommentController {
+    @Autowired
+    CommentRepository commentRepository;
+
+    @GetMapping("/api/fetch/comment/{id}") 
+    public ResponseEntity<Comment> getCommentById(HttpServletRequest request, @PathVariable Integer id) {
+        Comment comment;
+        HttpStatus response;
+
+        if (commentRepository.existsById(id)) {
+            System.out.println("Comment with id: " + id);
+            comment = commentRepository.findById(id).get();
+            response = HttpStatus.OK;
+        } else {
+            System.out.println("Could not find comment with id: " + id);
+            comment = null;
+            response = HttpStatus.NOT_FOUND;
+        }
+
+        return new ResponseEntity<>(comment, response);
+    }
+
+    @GetMapping("/api/fetch/comment/all")
+    public ResponseEntity<ArrayList<Comment>> getAllComments(HttpServletRequest request) {
+        ArrayList<Comment> comments = new ArrayList<Comment>();
+        HttpStatus response;
+
+        comments = (ArrayList<Comment>)commentRepository.findAll();
+        response = HttpStatus.OK;
+        System.out.println("Fetched all comments");
+
+        return new ResponseEntity<>(comments, response);
+    }
+
+    @PostMapping("/api/create/comment")
+    public ResponseEntity<Comment> addComment(HttpServletRequest request, @RequestBody Comment newComment) {
+        newComment = commentRepository.save(newComment);
+        System.out.println("New comment with id: " + newComment.getId() + " added");
+        HttpStatus response = HttpStatus.CREATED;
+        return new ResponseEntity<>(newComment, response);
+    }
+
+    @PatchMapping("/api/update/comment/{id}")
+    public ResponseEntity<Comment> updateComment(HttpServletRequest request, @RequestBody Comment newComment, @PathVariable Integer id) {
+        Comment comment;
+        HttpStatus response;
+
+        if (commentRepository.existsById(id)) {
+            comment = commentRepository.findById(id).get();
+
+            if (newComment.getMessage() != null) {
+                comment.setMessage(newComment.getMessage());
+            }
+            if (newComment.getPostId() != null) {
+                comment.setPostId(newComment.getPostId());
+            }
+            if (newComment.getUserAccountId() != null) {
+                comment.setUserAccountId(newComment.getUserAccountId());
+            }
+
+            commentRepository.save(comment);
+            response = HttpStatus.OK;
+            System.out.println("Updated comment with id: " + comment.getId());
+        } else {
+            System.out.println("Could not find comment with id: " + id);
+            comment = null;
+            response = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(comment, response);
+    }
+
+    @DeleteMapping("/api/delete/comment/{id}")
+    public ResponseEntity<String> deteleComment(HttpServletRequest request, @PathVariable Integer id) {
+        String message = "";
+        HttpStatus response;
+
+        if (commentRepository.existsById(id)) {
+            commentRepository.deleteById(id);
+            System.out.println("Deleted comment with id: " + id);
+            message = "SUCCESS";
+            response = HttpStatus.OK;
+        } else {
+            System.out.println("Could not find comment with id: " + id);
+            message = "FAIL";
+            response = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(message, response);
+    }
+}
---
+++ src/main/java/experis/noticeboard/models/Comment.java
@@ -0,0 +1,67 @@
+package experis.noticeboard.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Entity
+@JsonIdentityInfo(
+    generator = ObjectIdGenerators.PropertyGenerator.class,
+    property = "id")
+public class Comment {
+ 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String message;
+
+    @Column
+    private Integer postId;
+
+    @Column
+    private Integer userAccountId;
+
+    public Comment() {
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    public Integer getUserAccountId() {
+        return userAccountId;
+    }
+
+    public void setUserAccountId(Integer userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+    
+}

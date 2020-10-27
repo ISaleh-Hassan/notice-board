913fef729e6f74cf2de82fd4873a52b585954c12
Heso113 <hampe_olsson91@hotmail.com>
2020-10-27 09:33:40 +0100

    refactoring and new features
    Refactoring:
    -created new folder called api in the controllers folder.
    -moved UserAccountController.java to api folder.
    
    New:
    -new model called post.
    -new controller called PostController
    -new Repository called PostRepository

---
+++ src/main/java/experis/noticeboard/controllers/api/PostController.java
@@ -0,0 +1,107 @@
+package experis.noticeboard.controllers.api;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import experis.noticeboard.models.Post;
+import experis.noticeboard.repositories.PostRepository;
+
+@RestController
+public class PostController {
+    @Autowired
+    PostRepository postRepository;
+
+    @GetMapping("/api/fetch/post/{id}") 
+    public ResponseEntity<Post> getPostById(HttpServletRequest request, @PathVariable Integer id) {
+        Post post;
+        HttpStatus response;
+
+        if (postRepository.existsById(id)) {
+            System.out.println("Post with id: " + id);
+            post = postRepository.findById(id).get();
+            response = HttpStatus.OK;
+        } else {
+            System.out.println("Could not find post with id: " + id);
+            post = null;
+            response = HttpStatus.NOT_FOUND;
+        }
+
+        return new ResponseEntity<>(post, response);
+    }
+
+    @GetMapping("/api/fetch/post/all")
+    public ResponseEntity<ArrayList<Post>> getAllPosts(HttpServletRequest request) {
+        ArrayList<Post> posts = new ArrayList<Post>();
+        HttpStatus response;
+
+        posts = (ArrayList<Post>)postRepository.findAll();
+        response = HttpStatus.OK;
+        System.out.println("Fetched all posts");
+
+        return new ResponseEntity<>(posts, response);
+    }
+
+    @PostMapping("/api/create/post")
+    public ResponseEntity<Post> addPost(HttpServletRequest request, @RequestBody Post newPost) {
+        newPost = postRepository.save(newPost);
+        System.out.println("New post with id: " + newPost.getId() + " added");
+        HttpStatus response = HttpStatus.CREATED;
+        return new ResponseEntity<>(newPost, response);
+    }
+
+    @PatchMapping("/api/update/post/{id}")
+    public ResponseEntity<Post> updatePost(HttpServletRequest request, @RequestBody Post newPost, @PathVariable Integer id) {
+        Post post;
+        HttpStatus response;
+
+        if (postRepository.existsById(id)) {
+            post = postRepository.findById(id).get();
+
+            if (newPost.getMessage() != null) {
+                post.setMessage(newPost.getMessage());
+            }
+            if (newPost.getUserAccountId() != null) {
+                post.setUserAccountId(newPost.getUserAccountId());
+            }
+
+            postRepository.save(post);
+            response = HttpStatus.OK;
+            System.out.println("Updated post with id: " + post.getId());
+        } else {
+            System.out.println("Could not find post with id: " + id);
+            post = null;
+            response = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(post, response);
+    }
+
+    @DeleteMapping("/api/delete/post/{id}")
+    public ResponseEntity<String> detelePost(HttpServletRequest request, @PathVariable Integer id) {
+        String message = "";
+        HttpStatus response;
+
+        if (postRepository.existsById(id)) {
+            postRepository.deleteById(id);
+            System.out.println("Deleted post with id: " + id);
+            message = "SUCCESS";
+            response = HttpStatus.OK;
+        } else {
+            System.out.println("Could not find post with id: " + id);
+            message = "FAIL";
+            response = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(message, response);
+    }
+}
